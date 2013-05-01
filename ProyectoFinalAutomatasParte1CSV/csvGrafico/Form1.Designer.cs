c6f70f5b5b0ac163cda4887767f2c1939c63dd37
Javier T Meza C <javiermeza.cazarez@gmail.com>
2013-05-01 11:43:54 -0700

    Parte 1 Proyecto Final Teoria de  Automantas
    El archivo csv que se genera manualmente debe ir sin una secuencia de
    espace al final de cada linea

---
+++ ProyectoFinalAutomatasParte1CSV/csvGrafico/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace csvGrafico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblString = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGuardado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buscar archivo:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(113, 4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // lblString
+            // 
+            this.lblString.AutoSize = true;
+            this.lblString.Location = new System.Drawing.Point(29, 49);
+            this.lblString.Name = "lblString";
+            this.lblString.Size = new System.Drawing.Size(83, 13);
+            this.lblString.TabIndex = 4;
+            this.lblString.Text = "string generado:";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(115, 85);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 5;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Genearr archivo:";
+            // 
+            // lblGuardado
+            // 
+            this.lblGuardado.AutoSize = true;
+            this.lblGuardado.Location = new System.Drawing.Point(302, 95);
+            this.lblGuardado.Name = "lblGuardado";
+            this.lblGuardado.Size = new System.Drawing.Size(123, 13);
+            this.lblGuardado.TabIndex = 7;
+            this.lblGuardado.Text = "Guardado Exitosamente!";
+            this.lblGuardado.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 120);
+            this.Controls.Add(this.lblGuardado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.lblString);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblString;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGuardado;
+    }
+}
+
---
+++ ProyectoFinalAutomatasParte1CSV/csvGrafico/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace csvGrafico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+        public string output = string.Empty;
+        #endregion
+
+        #region Eventos
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //buscar archivo para leerlo
+            //"C:\\Users\\JavierTMezaC\\Desktop\\csv.txt"
+            try
+            {
+                var reader = new StreamReader(File.OpenRead(ReturnsPathOpenFileDialog()));
+                while (!reader.EndOfStream) // mientras haya lectura
+                {
+                    //lee una linea completa
+                    var line = reader.ReadLine();
+
+                    //regresa cada valor de la linea en un arreglo
+                    var arrayValues = line.Split(',');
+
+                    // recorrer cada elemento del arrayValues
+                    for (int i = 0; i < arrayValues.Length; i++)
+                    {
+                        output += AddZeros(Convert.ToInt32(arrayValues[i]));
+                        if (i != (arrayValues.Length - 1))
+                            output += "1";
+                        else if (!reader.EndOfStream)
+                            output += "11";
+                    }
+                }// end while mientras haya lectura
+                lblString.Text = output;
+            }
+            catch (Exception erroEX)
+            {
+                MessageBox.Show(erroEX.Message, erroEX.GetType().ToString());
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (GuardarString(output))
+                    lblGuardado.Visible = true;
+            }
+            catch (Exception errorEx)
+            {
+                MessageBox.Show(errorEx.Message, errorEx.GetType().ToString());
+            }
+        }
+        #endregion
+
+        #region Metodos Privados
+        private static string ReturnsPathOpenFileDialog()
+        {
+            try
+            {     
+                OpenFileDialog file = new OpenFileDialog();
+                file.InitialDirectory = "c:\\Users\\JavierTMezaC\\Desktop";
+                file.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                file.FilterIndex = 2;
+                file.RestoreDirectory = true;
+                file.ShowDialog();
+                return file.FileName;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        private static string AddZeros(int number)
+        {
+            string output = string.Empty;
+            switch (number)
+            {
+                case 1:
+                    output = "0";
+                    break;
+                case 2 :
+                    output = "00";
+                    break;
+                case 3:
+                    output = "000";
+                    break;
+                case 4:
+                    output =  "0000";
+                    break;
+                case 5:
+                    output =  "00000";
+                    break;
+                case 6:
+                    output = "000000";
+                    break;
+                case 7:
+                    output = "0000000";
+                    break;
+                case 8:
+                    output = "00000000";
+                    break;
+                case 9:
+                    output = "000000000";
+                    break;
+                default:
+                    break;
+            }
+            return output;
+        }
+
+        private static bool GuardarString(string cadena)
+        {
+            try
+            {
+                SaveFileDialog ToSave = new SaveFileDialog();
+                ToSave.Title = "Save the string generated";
+                ToSave.DefaultExt = "ctm";
+                ToSave.AddExtension = true;
+                ToSave.ShowDialog();
+                File.AppendAllText(ToSave.FileName, cadena);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
